1798a80c3af25af09c343ac3b6bf244e36598d1b
furaha707 <zxcvyein950823@gmail.com>
2023-05-30 13:38:46 +0900

    Initial commit

---
+++ kor/include/arr_data.php
@@ -0,0 +1,151 @@
+<?
+
+$site_title = "LOTTE R&D CENTER"; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = "(주)롯데중앙연구소"; // 회사명
+
+$P_addr1 = "서울특별시 강서구 마곡중앙로 201"; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "02-6309-3114";
+$P_tel2 = "";
+
+$P_fax1 = "02-6309-3099";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = ""; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "연구소 소개";
+$nav_1_1 = "롯데중앙연구소";
+$nav_1_1_eng = "LOTTE R&D CENTER";
+$nav_1_1_1 = "회사소개";
+$nav_1_1_2 = "Mission & Vision";
+$nav_1_2 = "연혁";
+$nav_1_2_eng = "HISTORY";
+$nav_1_3 = "CEO Message";
+$nav_1_3_eng = "CEO MESSAGE";
+$nav_1_4 = "오시는 길";
+$nav_1_4_eng = "LOCATION";
+
+$nav_2 = "연구활동";
+$nav_2_1 = "연구분야";
+$nav_2_1_eng = "R&D AREAS";
+$nav_2_1_1 = "연구개발";
+$nav_2_1_2 = "기초연구";
+$nav_2_1_3 = "패키지연구";
+$nav_2_1_4 = "기술연구";
+$nav_2_1_5 = "안전관리";
+$nav_2_1_6 = "연구전략";
+$nav_2_2 = "연구성과";
+$nav_2_2_eng = "R&D RESULTS";
+$nav_2_2_1 = "Better Food";
+$nav_2_2_2 = "Better Life";
+$nav_2_3 = "논문 및 특허";
+$nav_2_3_eng = "THESIS AND PATENTS";
+$nav_2_3_1 = "논문";
+$nav_2_3_2 = "특허";
+$nav_2_4 = "수상 및 인증";
+$nav_2_4_eng = "AWARDS AND CERTIFICATIONS";
+$nav_2_4_1 = "수상";
+$nav_2_4_2 = "인증";
+
+$nav_3 = "홍보센터";
+$nav_3_1 = "공지사항";
+$nav_3_1_eng = "NOTICE";
+$nav_3_2 = "보도자료";
+$nav_3_2_eng = "NEWS";
+$nav_3_3 = "Museum L";
+
+$nav_4 = "인재채용";
+$nav_4_1 = "채용정보";
+$nav_4_1_eng = "RECRUITMENT INFORMATION";
+$nav_4_1_1 = "인재상";
+$nav_4_1_2 = "인사제도";
+$nav_4_1_3 = "복리후생";
+$nav_4_1_4 = "채용 프로세스";
+$nav_4_2 = "People Interview";
+$nav_4_2_eng = "PEOPLE INTERVIEW";
+$nav_4_3 = "입사지원";
+$nav_4_3_eng = "JOB APPLICATION";
+
+$nav_5 = "고객센터";
+$nav_5_1 = "고객문의";
+$nav_5_1_eng = "Q&A";
+$nav_5_2 = "교육신청";
+$nav_5_2_eng = "EDUCATION";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "영상정보처리기기 운영방침";
+$nav_policy_2 = "개인정보처리방침";
+$nav_policy_3 = "이메일주소무단수집거부";
+$nav_policy_4 = "이용약관";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../about/introduce__company.html";
+$nav_1_1_href = "../about/introduce__company.html";
+$nav_1_1_1_href = "../about/introduce__company.html";
+$nav_1_1_2_href = "../about/introduce__mission.html";
+$nav_1_2_href = "../about/history.html";
+$nav_1_3_href = "../about/ceo.html";
+$nav_1_4_href = "../about/location.html";
+
+$nav_2_href = "../research/field__rnd.html";
+$nav_2_1_href = "../research/field__rnd.html";
+$nav_2_1_1_href = "../research/field__rnd.html";
+$nav_2_1_2_href = "../research/field__basic.html";
+$nav_2_1_3_href = "../research/field__package.html";
+$nav_2_1_4_href = "../research/field__tech.html";
+$nav_2_1_5_href = "../research/field__safety.html";
+$nav_2_1_6_href = "../research/field__strategy.html";
+$nav_2_2_href = "../research/better__food.html";
+$nav_2_2_1_href = "../research/better__food.html";
+$nav_2_2_2_href = "../research/better__life.html";
+$nav_2_3_href = "../research/board__thesis.html";
+$nav_2_3_1_href = "../research/board__thesis.html";
+$nav_2_3_2_href = "../research/board__patents.html";
+$nav_2_4_href = "../research/gallery__awards.html";
+$nav_2_4_1_href = "../research/gallery__awards.html";
+$nav_2_4_2_href = "../research/gallery__certificate.html";
+
+$nav_3_href = "../notice/notice.html";
+$nav_3_1_href = "../notice/notice.html";
+$nav_3_2_href = "../notice/media.html";
+$nav_3_3_href = "../notice/museum.html";
+
+$nav_4_href = "../recruit/recruit__talent.html";
+$nav_4_1_href = "../recruit/recruit__talent.html";
+$nav_4_1_1_href = "../recruit/recruit__talent.html";
+$nav_4_1_2_href = "../recruit/recruit__personsystem.html";
+$nav_4_1_3_href = "../recruit/recruit__benefit.html";
+$nav_4_1_4_href = "../recruit/recruit__process.html";
+$nav_4_2_href = "../recruit/interview.html";
+$nav_4_3_href = "../recruit/apply.html";
+
+$nav_5_href = "../inquiry/customer.html";
+$nav_5_1_href = "../inquiry/customer.html";
+$nav_5_2_href = "../inquiry/education.html";
+
+$nav_policy_href = "../policy/video-security.html";
+$nav_policy_1_href = "../policy/video-security.html";
+$nav_policy_2_href = "../policy/privacy.html";
+$nav_policy_3_href = "../policy/email-security.html";
+$nav_policy_4_href = "../policy/term.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+ini_set( "display_errors", 0 );
+
+?>
